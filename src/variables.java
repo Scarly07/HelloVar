915a3d90a773e8bb698e50597070d9a0cff0a78a
Tim Gotje <timgotje@lt20439.local>
2018-09-27 10:04:24 +0200

    Initial commit

---
+++ src/variables.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class variables {
+
+     public static void main (String[] args) {
+
+         String name = "Tim";
+         String achternaam = "Gotjé";
+         int leerjaar = 1;
+         double number = 2.2;
+
+         System.out.println(name + " " + achternaam);
+      System.out.println("Hello variables");
+
+         Scanner scan = new Scanner(System.in);
+         name = scan.next();
+         System.out.println(name);
+
+
+
+                }
+
+}
